068ba33cb4f6bf210f49a363edba733abc63c472
Viola <asdfg2009-29-69@mail.ru>
2022-09-17 12:07:43 +0300

    .

--- дз/Program.cs
+++ дз/Program.cs
@@ -2,16 +2,32 @@
 //  длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 //  либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 //  лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
 
-
-Console.Clear();
-
-string[] array = { "hello", "2", "world", ":-)" };
-string[] newarr = new string[array.Length];
-
-for (int i = 0; i < array.Length; i++)
+string[] array1 = new string[11] {"1234", "2", "hello", "world", ":-)","1567", "-2", "computer science", "Russia", "Denmark","Kazan"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
-    if (array[i].Length <= 3)
-        newarr[i] = array[i];
-    Console.Write($"{newarr[i]} ");
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
